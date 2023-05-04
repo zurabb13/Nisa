f20b58f2398364517a7536c7bd8c7e9b942172ab
zura <zura.baramia98@outlook.com>
2023-05-04 17:48:05 +0400

    mobile - about: client-details

--- src/app/components/about/about.module.ts
+++ src/app/components/about/about.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
@@ -7,11 +7,19 @@ import { SubtitleComponent } from './subtitle/subtitle.component';
 import { MeetSectionComponent } from './meet-section/meet-section.component';
 import { HttpClientModule } from '@angular/common/http';
 import { register } from 'swiper/element/bundle';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { MaterialModule } from '../../shared/module/material.module';
 
 register();
 
 @NgModule({
-  declarations: [AboutComponent, SubtitleComponent, MeetSectionComponent],
-  imports: [CommonModule, AboutRoutingModule, HttpClientModule],
+  declarations: [
+    AboutComponent,
+    SubtitleComponent,
+    MeetSectionComponent,
+    ClientDetailsComponent,
+  ],
+  imports: [CommonModule, AboutRoutingModule, HttpClientModule, MaterialModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AboutModule {}
